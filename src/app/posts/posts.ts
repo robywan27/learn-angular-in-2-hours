4a127a5ab6d9d8cd91c0d008bbbbc7f91f6505bb
robywan27 <my-subs@tuta.io>
2026-03-25 16:19:24 +0100

    Add two-way binding for Post button

--- src/app/posts/posts.ts
+++ src/app/posts/posts.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './posts.html',
   styleUrl: './posts.scss',
 })
 export class Posts {
   @Input() userPosts: any[] = [];
   likedPosts: Set<number> = new Set();
+  newComment: string = '';
 
   likePost(id: number): void {
     if (this.likedPosts.has(id)) {
@@ -17,6 +19,14 @@ export class Posts {
     }
     else {
       this.likedPosts.add(id);
+    }
+  }
+
+  addComment(postId: number): void {
+    const post = this.userPosts.find(p => p.id === postId);
+    if (post && this.newComment.trim()) { // check post is not undefined before checking if new comment is not empty
+      post.comments.push(this.newComment);
+      this.newComment = '';
     }
   }
 }
