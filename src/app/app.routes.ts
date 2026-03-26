bd26fbbf90b52993f923d04efa06f2cd1c47923d
robywan27 <my-subs@tuta.io>
2026-03-26 16:51:20 +0100

    Add route for / endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Posts } from './posts/posts';
 import { Profile } from './profile/profile';
+import { Navigation } from './navigation/navigation';
 
 export const routes: Routes = [
+    {path: '', component: Navigation},
     {path: 'posts', component: Posts},
     {path: 'profile', component: Profile},
 ];
